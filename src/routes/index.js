fc9e5012b14b2a27de4b1f1af85a951d96b61482
Cosmo DeHaan <cosmodon411974@gmail.com>
2023-09-07 08:57:16 -0400

    test env

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { indexPage } from '../controllers';
+import { messagesPage } from '../controllers/messages';
+import { indexPage } from '../controllers/home';
+
 const indexRouter = express.Router();
+
+indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
